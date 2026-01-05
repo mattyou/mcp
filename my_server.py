bf97f34418d49f5ab989234bad9391b1a66f20d0
youyifeng <youyifeng@zhulong.com>
2026-01-05 13:30:56 +0800

    upate something

--- my_server.py
+++ my_server.py
@@ -1,6 +1,9 @@
 from fastmcp import FastMCP
 
-mcp = FastMCP("My MCP Server")
+mcp = FastMCP(
+    name="My MCP Server",
+    instructions="这是一个欢迎MCP服务器，你可以使用它来调用greet工具。用户输入的名字，服务器将返回一个欢迎消息。"
+)
 
 @mcp.tool
 def greet(name: str) -> str:
